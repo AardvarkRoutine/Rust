b49996d4027389cb665b3761c07043c79c2caa19
Paul <paul.schworm@fohren-linden.de>
2025-03-23 17:05:08 +0100

    Aufgabe 4

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,6 @@ pub fn say_hello() -> String {
     "Hello, tests!".to_string()
 }
 
-
-
 // Aufgabe 2
 pub fn greet(name: &str) -> String {
     format!("Hello, {}!", name)
@@ -15,6 +13,22 @@ pub fn parse_number(input: &str) -> Result<u32, String>  {
     match input.trim().parse::<u32>() {
         Ok(n) => Ok(n),
         Err(_) => Err(format!("'{}' ist keine gültige Zahl!", input))
+    }
+}
+
+//Aufgabe 4
+pub enum Action {
+    Idle,
+    Jump(u32),
+    Speak(String),
+    Dash {x:i32, y: i32}
+}
+pub fn describe(action:Action) -> String {
+    match action {
+        Action::Idle => "Steht nur rum!".to_string(),
+        Action::Jump(height ) => format!("springt {} hoch!", height),
+        Action::Speak(message) => format!("Sagt: {}", message),
+        Action::Dash { x, y } => format!("flitzt nach ({}, {})", x, y)  
     }
 }
 
@@ -42,14 +56,20 @@ mod tests {
         let result = parse_number("42");
         assert!(result.is_ok());
         assert_eq!(result.unwrap(), 42);
-}
+    }
 
-#[test]
+    #[test]
     fn fails_on_invalid_input() {
         let result = parse_number("abc");
         assert!(result.is_err());
         assert_eq!(result.unwrap_err(), "'abc' ist keine gültige Zahl");
-}
+    }
 
-
+    #[test]
+    fn test_describe() {
+        assert_eq!(describe(Action::Idle), "steht nur rum");
+        assert_eq!(describe(Action::Jump(10)), "springt 10 hoch");
+        assert_eq!(describe(Action::Speak("Hallo".to_string())), "sagt: Hallo");
+        assert_eq!(describe(Action::Dash { x: 1, y: -1 }), "flitzt nach (1, -1)");
+    }
 }
